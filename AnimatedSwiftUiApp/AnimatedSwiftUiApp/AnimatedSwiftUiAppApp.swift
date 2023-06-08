a1daadfeea420f030cfde3acb5938fdf7b5ab5e4
mathcosfelipe <mfbdcarvalho@gmail.com>
2023-06-07 23:16:07 -0300

    first commit

---
+++ AnimatedSwiftUiApp/AnimatedSwiftUiApp/AnimatedSwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSwiftUiAppApp.swift
+//  AnimatedSwiftUiApp
+//
+//  Created by Marcos Carvalho on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
